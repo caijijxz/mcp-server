a24544b16ef3f9edba9f2fc77ad3cbbd552d7186
jiangxz <2624495623@qq.com>
2026-03-03 16:05:58 +0800

    test1

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ mcp = FastMCP("My MCP Server")
 
 @mcp.tool
 def greet(name: str) -> str:
-    return f"Hello, {name}!"
+    return f"Hello, bro, {name}!"
 
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
